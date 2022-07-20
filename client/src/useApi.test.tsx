3f3336d6eb25203dff703ec6255d0d770967adee
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-20 12:47:25 -0500

    created useApi function and test

---
+++ client/src/useApi.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+//https://mswjs.io/docs/getting-started/install
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitFor, renderHook, act } from '@testing-library/react';
+import { useApi } from './useApi';
+
+const server = setupServer(
+    rest.get('/api', (req, res, ctx) => {
+        return res(ctx.json({ name: 'Amber' }));
+    })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("Should return data", async () => {
+    const { result } = renderHook(() => useApi());;
+    await waitFor(() =>
+        expect(result.current).toStrictEqual({ name: 'Amber' }));
+});
