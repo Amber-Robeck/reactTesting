bd4cb267cea7cc24a4bdb823328bf597015c0edb
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-20 12:27:08 -0500

    Added useCounter component and test

---
+++ client/src/useCounter.test.tsx
@@ -0,0 +1,11 @@
+import { renderHook, act } from '@testing-library/react'
+import { useCounter } from './useCounter';
+
+test("should increment the count", () => {
+    const { result } = renderHook(() => useCounter());
+    act(() => {
+        result.current.increment();
+    }
+    );
+    expect(result.current.count).toBe(1);
+});
